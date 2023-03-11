793a0d298c1e4d39f13197b415266bf13892a3ff
Torben <59419684+entorb@users.noreply.github.com>
2023-03-11 09:15:23 +0100

    adding and fixing pre-commit hook

--- shuffle-music/shuffle_music_lib.py
+++ shuffle-music/shuffle_music_lib.py
@@ -1,22 +1,29 @@
+#!/usr/bin/env python3
+"""
+Shared methods.
+"""
+
 
 def check_all_played(total_songs: int, playorder: list) -> bool:
-    """ 
-    checks if all songs are played = in playorder
+    """
+    Check if all songs are played = in playorder.
+
     returns true if so, else false
     """
-    for song in range(total_songs):
-        if song not in playorder:
-            return False
-    return True
+    # for song in range(total_songs):
+    #     if song not in playorder:
+    #         return False
+    # return True
+    return all(not song not in playorder for song in range(total_songs))
 
 
-assert check_all_played(6, (0, 1, 2, 3, 4, 5)) == True
-assert check_all_played(6, (5, 4, 3, 2, 1, 0)) == True
-assert check_all_played(6, (0, 1, 2, 3, 4, 0)) == False
+assert check_all_played(6, (0, 1, 2, 3, 4, 5)) is True
+assert check_all_played(6, (5, 4, 3, 2, 1, 0)) is True
+assert check_all_played(6, (0, 1, 2, 3, 4, 0)) is False
 
 
 def check_cnt_unique_played(playorder: list) -> int:
-    """ counts how many songs are played """
+    """Count how many songs are played."""
     how_many_played = set(playorder)  # set is a unique list
     return len(how_many_played)
 
