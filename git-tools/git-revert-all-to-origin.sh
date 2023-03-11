c8010562151f8be49fc44cde97b26e7d807cf672
Torben <59419684+entorb@users.noreply.github.com>
2023-03-11 09:15:22 +0100

    Create git-revert-all-to-origin.sh

---
+++ git-tools/git-revert-all-to-origin.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+for D in `ls -d */`
+do 
+    echo ===
+    echo === $D ====
+    echo ===
+    cd $D
+    git fetch origin
+    git reset --hard origin/main
+    git pull 
+    cd ..
+done
+
+
