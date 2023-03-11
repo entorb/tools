219b298bd37734b36061a1830e901c0f2d6c0f99
Torben <59419684+entorb@users.noreply.github.com>
2023-03-11 09:15:23 +0100

    Update cartesian-csv-gen.py

--- cartesian-csv-gen/cartesian-csv-gen.py
+++ cartesian-csv-gen/cartesian-csv-gen.py
@@ -4,8 +4,8 @@
 Cartesian product of dynamic parameters.
 
 reads config.ini
-extracts dynamic parameters (list of values per parameter, sep. by ," )
-generates cartesian product of dynamic parameters
+extracts dynamic parameters (list of values per parameter, separated by ',' )
+generates cartesian product of all dynamic parameter combinations
 """
 import itertools
 from configparser import ConfigParser
@@ -13,21 +13,21 @@ from configparser import ConfigParser
 config = ConfigParser()
 config.read("cartesian-csv-gen.ini")
 
-listOfKeys: list[str] = []
-listsOfValues: list[str] = []
+list_keys: list[str] = []
+list_values: list[list[str]] = []
 
 for k in config.options("dynamic"):
     s = config.get("dynamic", k)
-    listOfKeys.append(k)
+    list_keys.append(k)
     l = s.split(",")
     l = [v.strip() for v in l]  # trim white spaces
-    listsOfValues.extend(l)
+    list_values.append(l)
 
 with open("out.csv", "w", newline="\n") as fh:
-    fh.write("\t".join(listOfKeys))
+    fh.write("\t".join(list_keys))
     fh.write("\n")
 
-    # Cartesian product of the listsOfDynVars
-    for combination in itertools.product(*listsOfValues):
+    # Cartesian product
+    for combination in itertools.product(*list_values):
         fh.write("\t".join(combination))
         fh.write("\n")
