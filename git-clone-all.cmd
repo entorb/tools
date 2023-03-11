8dd461e7ee91ed2dd49c7cd248f07b79c5882e1d
Torben <59419684+entorb@users.noreply.github.com>
2023-03-11 09:15:20 +0100

    new repo

--- git-clone-all.cmd
+++ git-clone-all.cmd
@@ -5,7 +5,7 @@ REM first open and unlock the SSH Key for the computer via Putty Pageant
 D:
 cd D:\GitHub
 
-FOR %%D in (arduino-sensorics, COVID-19-Coronavirus-German-Regions, HackerRank, private, raspi-sensorics, rememberthemilk, sql2csv, tools, tools-backup-scripts, tools-photos, twitter-gov-accounts) DO (
+FOR %%D in (arduino-sensorics, COVID-19-Coronavirus-German-Regions, dissertation, HackerRank, private, raspi-sensorics, rememberthemilk, sql2csv, tools, tools-backup-scripts, tools-photos, twitter-gov-accounts) DO (
 
 echo ===
 echo === %%D ====
