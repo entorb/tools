7785fba463705c1d216abc708b5ffb65791c6227
Torben <59419684+entorb@users.noreply.github.com>
2023-03-11 09:15:23 +0100

    type hints

--- shuffle-music/shuffle_music_lib.py
+++ shuffle-music/shuffle_music_lib.py
@@ -4,7 +4,7 @@ Shared methods.
 """
 
 
-def check_all_played(total_songs: int, playorder: list) -> bool:
+def check_all_played(total_songs: int, playorder: tuple[int]) -> bool:
     """
     Check if all songs are played = in playorder.
 
@@ -22,11 +22,11 @@ assert check_all_played(6, (5, 4, 3, 2, 1, 0)) is True
 assert check_all_played(6, (0, 1, 2, 3, 4, 0)) is False
 
 
-def check_cnt_unique_played(playorder: list) -> int:
+def check_cnt_unique_played(playorder: tuple[int]) -> int:
     """Count how many songs are played."""
-    how_many_played = set(playorder)  # set is a unique list
+    how_many_played: set[int] = set(playorder)  # set is a unique list
     return len(how_many_played)
 
 
-assert check_cnt_unique_played((0, 1, 2, 3, 4, 5)) == 6
-assert check_cnt_unique_played((0, 0, 2, 2, 4, 5)) == 4
+assert check_cnt_unique_played(playorder=(0, 1, 2, 3, 4, 5)) == 6
+assert check_cnt_unique_played(playorder=(0, 0, 2, 2, 4, 5)) == 4
