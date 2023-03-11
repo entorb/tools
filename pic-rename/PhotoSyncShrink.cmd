3ac271f53a782b2ee415c7e231b4ad08cd358076
Torben <59419684+entorb@users.noreply.github.com>
2023-03-11 09:15:20 +0100

    created

---
+++ pic-rename/PhotoSyncShrink.cmd
@@ -0,0 +1,11 @@
+@echo off
+REM PhotoSyncShrink.pl 2018
+PhotoSyncShrink.pl 2019
+PhotoSyncShrink.pl 2020
+REM PhotoSyncShrink.pl 2017
+REM PhotoSyncShrink.pl 2016
+REM PhotoSyncShrink.pl 2015
+REM PhotoSyncShrink.pl 2014
+
+
+pause
