ab2e9bceb9ac0221edb7db1fe2c3e70c583a2d1d
Torben <59419684+entorb@users.noreply.github.com>
2024-11-07 12:42:02 +0100

    Pathlib

--- UTF8_to_ANSI/convert_utf8_to_ansi.py
+++ UTF8_to_ANSI/convert_utf8_to_ansi.py
@@ -3,32 +3,31 @@
 Convert UTF-8 to ANSI.
 """
 
-# TODO:
-# ruff: noqa
-
-import glob
-import os
+import sys
+from pathlib import Path
 
 for d in ("in", "out", "done", "error"):
-    os.makedirs(d, exist_ok=True)  # = mkdir -p
+    p = Path(d)
+    p.mkdir(exist_ok=True)
 
-for f in glob.glob("in/*.*"):
-    (filepath, fileName) = os.path.split(f)
-    print(fileName)
+for f in Path("in").glob("*.*"):
+    print(f.name)
 
     try:
-        with open(f, encoding="utf-8") as fh:
+        with f.open(encoding="utf-8") as fh:
             cont = fh.read()
     except UnicodeEncodeError:
-        os.rename(f, f"error/{fileName}")
-        print(f"ERROR reading '{fileName}' as UTF-8")
-        exit()
+        f.rename(f"error/{f.name}")
+        print(f"ERROR reading '{f.name}' as UTF-8")
+        sys.exit()
 
     try:
-        with open(f"out/{fileName}", mode="w", encoding="ansi", newline="\r\n") as fh:
+        with Path(f"out/{f.name}").open(
+            mode="w", encoding="ansi", newline="\r\n"
+        ) as fh:
             fh.write(cont)
-        os.rename(f, f"done/{fileName}")
+        f.rename(f"done/{f.name}")
     except UnicodeEncodeError:
-        os.rename(f, f"error/{fileName}")
-        print(f"ERROR converting '{fileName}' to ANSI")
-        os.remove(f"out/{fileName}")
+        f.rename(f"error/{f.name}")
+        print(f"ERROR converting '{f.name}' to ANSI")
+        Path(f"out/{f.name}").unlink()
