83b34f361eff476d0ed356645760d1970c1e2749
Torben <59419684+entorb@users.noreply.github.com>
2026-01-24 16:11:27 +0100

    git-all-repo-hist-cleanup.py
    Update git-all-repo-hist-cleanup.py

--- git-tools/git-all-repo-modify.sh
+++ git-tools/git-all-repo-modify.sh
@@ -1,63 +1,58 @@
 #!/bin/bash
 
-for D in $(ls -d */); do
-    # skip dirs starting with zzz_
+# exit upon error
+set -e
+
+for D in */; do
+    # skip dirs starting with zzz_ as well as hpmor-de/
     case $D in
-    zzz_*) continue ;;
+        zzz_*/|hpmor-de/) continue ;;
     esac
-    # if [ $D == "template-python/" ]; then continue; fi
-    # if [ $D == "hpmor-de/" ]; then continue; fi
 
     echo ===
     echo === $D ====
     echo ===
-    if [ -f $D/ruff.toml ]; then
-        cd $D
-        # ruff check --fix
-        # ruff format
-        git add .pre-commit-config.yaml
-        pre-commit run --all-files
-        # git commit -m "Update .pre-commit-config.yaml"
-        # pre-commit autoupdate
-        # pre-commit run --all-files
-        # git add .github/workflows/*.yml
-        # git pull
-        # git commit -m "Use .python-version in GH Actions"
-        # git push
-        cd ..
-        # read -p "Press enter to continue"
-    fi
-    # if [ ! -f LICENSE ]; then
-    #     read -p "LICENSE missing, shall we add it? (y/n) " choice
+    cd $D
+    git checkout main
+    git pull
+    # if [ -f .pre-commit-config.yaml ]; then
+    #     pre-commit autoupdate
+    #     pre-commit run --all-files
+    #     git status
+    #     # read -p "commit changes? (y/n) " choice
+    #     # if [ "$choice" == "y" ]; then
+    #     git add .pre-commit-config.yaml
+    #     git commit -m "Update .pre-commit-config.yaml"
+    #     git push
+    #     # fi
+    # fi
+
+
+    # git add .github/workflows/*.yml
+    # git commit -m "GH Action sonarqube-scan-action@v7"
+    # git push
+
+    git add *.sh
+    git commit -m "Alignment deploy.sh update.sh"
+    git push
+
+    #     read -p "add .github/workflows (y/n) " choice
     #     if [ "$choice" == "y" ]; then
-    #         cp ../template-python/LICENSE ./
-    #         git add LICENSE
-    #         git commit -m "Add LICENSE file"
-    #         git push
+    #         mkdir -p .github/workflows
+    #         cp ../template/python/.github/workflows/check.yml
     #     fi
     # fi
 
-    # if [ -f setup.cfg ]; then
-    #     read -p "replace setup.cfg by .ruff.toml? (y/n) " choice
-    #     if [ "$choice" == "y" ]; then
-    #         cp ../template-python/.ruff.toml ./
-    #         cp ../template-python/.pre-commit-config.yaml ./
-    #         rm setup.cfg
-    #         # git add .ruff.toml
-    #         # git commit -m "Add .ruff.toml file"
-    #         # git push
-    #     fi
-    # fi
-
-    # if [ ! -f .pre-commit-config.yaml ]; then
+    # if [ ! -d .github ]; then
     #     read -p "add .pre-commit-config.yaml? (y/n) " choice
     #     if [ "$choice" == "y" ]; then
-    #         cp ../template-python/.ruff.toml ./
+    #         cp ../template-python/ruff.toml ./
     #         cp ../template-python/.pre-commit-config.yaml ./
-    #         # git add .ruff.toml
-    #         # git commit -m "Add .ruff.toml file"
+    #         # git add ruff.toml
+    #         # git commit -m "Add ruff.toml file"
     #         # git push
     #     fi
     # fi
-    # cd ..
+
+    cd ..
 done
