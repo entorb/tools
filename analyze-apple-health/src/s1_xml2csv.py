099aa44fde88e7f438990909934f4aafbe27620d
Torben <59419684+entorb@users.noreply.github.com>
2026-08-26 14:00:21 +0200

    analyze-apple-health

---
+++ analyze-apple-health/src/s1_xml2csv.py
@@ -0,0 +1,83 @@
+# by Dr. Torben Menke https://entorb.net
+# https://github.com/entorb/analyze-apple-health
+"""
+Read Apple health exported data.
+
+apple_health_export/export.xml
+export in csv and Excel format
+out/data-raw.*
+Note: Excel has a limit of 1,048,576 rows...
+
+based on https://towardsdatascience.com/analyse-your-health-with-python-and-apple-health-11c12894aae2
+TODO: checkout export_cda.xml too
+"""
+
+import time
+from pathlib import Path
+
+import pandas as pd
+from defusedxml.ElementTree import parse as XMLparse  # noqa: N812
+
+Path("out").mkdir(exist_ok=True)
+
+print("read xml data")
+timelast = time.time()
+tree = XMLparse("apple_health_export/export.xml")
+# For every health record, extract the attributes into a dictionary (columns).
+# Then create a list (rows).
+root = tree.getroot()
+record_list = [x.attrib for x in root.iter("Record")]
+print(f"{int(time.time() - timelast)}s")
+
+
+print("convert to DataFrame")
+timelast = time.time()
+# create DataFrame from a list (rows) of dictionaries (columns)
+df = pd.DataFrame(record_list)
+print(f"{int(time.time() - timelast)}s")
+
+
+print("column modifications")
+timelast = time.time()
+
+df.index.name = "row"
+
+# date fixes
+for col in ["creationDate", "startDate", "endDate"]:
+    # proper type to dates
+    df[col] = pd.to_datetime(df[col])
+    # Remove timezone from columns
+    df[col] = df[col].dt.tz_localize(tz=None)
+
+# convert value to numeric or NaN if fails
+df["value"] = pd.to_numeric(df["value"], errors="coerce")
+
+# how to treat nun-numeric values
+# a ) filling with 1.0 (= one time) makes it easier to aggregate
+# df["value"] = df["value"].fillna(1.0)
+# b) just drop value=na rows
+df = df[df["value"].notna()]
+
+# shorter observation names: use vectorized replace function
+df["type"] = df["type"].str.replace("HKQuantityTypeIdentifier", "")
+df["type"] = df["type"].str.replace("HKCategoryTypeIdentifier", "")
+print(f"{int(time.time() - timelast)}s")
+
+
+print("export out/data-raw2.tsv")
+timelast = time.time()
+df.to_csv(
+    "out/data-raw-2.tsv",
+    sep="\t",
+    lineterminator="\n",
+)
+print(f"{int(time.time() - timelast)}s")
+
+
+if len(df.index) >= 1048576 - 1:
+    print("too many rows for Excel export")
+else:
+    print("export out/data-raw-2.xlsx")
+    timelast = time.time()
+    df.to_excel("out/data-raw-2.xlsx")
+    print(f"{int(time.time() - timelast)}s")
---
+++ analyze-apple-health/src/s2_analyze.py
@@ -0,0 +1,112 @@
+# by Dr. Torben Menke https://entorb.net
+# https://github.com/entorb/analyze-apple-health
+"""
+Analyze data.
+
+read out/data-raw-2.tsv
+export example data to out/data-type-examples/
+convert to pivot table using endDate as index
+export month summary for certain data types
+"""
+
+from pathlib import Path
+
+import numpy as np
+import pandas as pd
+
+Path("out/data-type-examples").mkdir(exist_ok=True)
+
+df = pd.read_csv(
+    "out/data-raw-2.tsv",
+    sep="\t",
+    index_col=0,
+    parse_dates=["creationDate", "startDate", "endDate"],
+    dtype={
+        "type": str,
+        "sourceName": "str",
+        "sourceVersion": "str",
+        "unit": "str",
+        "value": "float64",
+        "device": "str",
+    },
+)
+
+print("export out/data-type-examples/")
+# print example rows per each type 1st, middle, last
+types = sorted(df["type"].unique().tolist())
+for t in types:
+    df2 = df[df["type"] == t]
+    lines = len(df2.index)
+    if lines >= 4:  # noqa: PLR2004
+        print(" - " + t)
+        df2 = df2.iloc[[0, int(lines / 3), int(2 * lines / 3), -1]]
+        df2.to_csv(
+            f"out/data-type-examples/{t}.tsv",
+            sep="\t",
+            line_terminator="\n",
+        )  # type: ignore
+
+
+df2 = df.groupby(["sourceName", "type"]).size().reset_index(name="count")
+print(df2)
+df2.to_csv(
+    "out/count_source_type.tsv",
+    sep="\t",
+    lineterminator="\n",
+    index=False,
+)
+
+df2 = df.groupby(["type", "sourceName"]).size().reset_index(name="count")
+print(df2)
+df2.to_csv(
+    "out/count_type_source.tsv",
+    sep="\t",
+    lineterminator="\n",
+    index=False,
+)
+
+# filter out useless data
+# SleepAnalysis: values = 1 -> means it has been done, but no data here
+df = df[~((df["type"] == "SleepAnalysis") & (df["value"] == 1))]
+# in the pivot I want to drop HeartRate data measured during my cycling activities
+df = df[~((df["type"] == "HeartRate") & (df["sourceName"] == "ELEMNT"))]
+
+
+# pivot
+df_pivot = df.pivot_table(index="endDate", columns="type", values="value")
+
+df_pivot.to_csv(
+    "out/pivot.tsv",
+    sep="\t",
+    lineterminator="\n",
+)
+
+# calc month average/mean values (without any filtering applied...)
+# uncomment data types you do not have in your health data set
+df_month = df_pivot.resample("M").agg(
+    {
+        "BodyMass": np.mean,
+        "StepCount": sum,
+        "FlightsClimbed": sum,
+        "DistanceWalkingRunning": sum,
+        "DistanceCycling": sum,
+        # "ActiveEnergyBurned": sum,
+        # "RestingHeartRate": np.mean,
+        "HeadphoneAudioExposure": np.mean,
+        # "HeartRate": np.mean, # needs filtering on activity and not
+        # "RespiratoryRate": np.mean,
+    },  # type: ignore
+)
+
+# round all
+# TODO: are digits for some columns needed?
+for col in df_month.columns:
+    df_month[col] = df_month[col].round(0)
+
+# export month summary
+df_month.to_csv(
+    "out/month.tsv",
+    sep="\t",
+    lineterminator="\n",
+)
+df_month.to_excel("out/month.xlsx")
